8420ca6100f4cf0958fadbae35b37ed012a41c3d
travis-racisz <tracisz14@gmail.com>
2025-05-13 21:03:37 -0500

    setting up GSAP

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useRef } from "react"
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
 import './App.css'
 
+
+gsap.registerPlugin(useGSAP);
+
 function App() {
-  const [count, setCount] = useState(0)
+  const container = useRef();
+  const circle = useRef();
+
+  useGSAP(
+    () => {
+      // use selectors...
+      gsap.to(".box", { rotation: "+=360", duration: 3 });
+
+      // or refs...
+      gsap.to(circle.current, { rotation: "-=360", duration: 3 });
+    },
+    { scope: container }
+  ); // <-- scope for selector text (optional)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div ref={container} className="container">
+        <div className="box gradient-blue">selector</div>
+        <div className="circle gradient-green" ref={circle}>
+          Ref
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="box gradient-blue">selector</div>
+    </div>
+  );
 }
 
 export default App
